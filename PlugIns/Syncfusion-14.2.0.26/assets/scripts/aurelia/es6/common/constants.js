7f28d2891239f79b46f64156c2ee86e6faab28aa
Brian Becker <brian@dyalog.com>
2016-10-04 17:53:43 -0400

    Remove Syncfusion 14.2.0.26

--- PlugIns/Syncfusion-14.2.0.26/assets/scripts/aurelia/es6/common/constants.js
+++
@@ -1,6 +0,0 @@
-export const constants = {
-  eventPrefix: 'e-on-',
-  bindablePrefix: 'e-',
-  attributePrefix: 'ej-',
-  elementPrefix: 'ej-'
-};
--- PlugIns/Syncfusion-14.2.0.26/assets/scripts/aurelia/es6/rangenavigator/rangeseries.js
+++
@@ -1,9 +0,0 @@
-import {inlineView, customElement, constants, generateBindables} from '../common/common';
-
-@inlineView('<template><content></content></template>')
-@customElement(`${constants.elementPrefix}range-series`)
-@generateBindables('series', [])
-
-export class RangeSeries {
-}
-
