ba006110c895360602ec753f55cd25b324ca4f37
Michael Baas <github@mail.mbaas.de>
2018-03-09 15:16:06 +0100

    Avoiding probs with circular refs when stringifying objects

--- PlugIns/JQuery/APL_JavaScript_Utils.js
+++ PlugIns/JQuery/APL_JavaScript_Utils.js
@@ -1,30 +1,44 @@
 function APLJaxReturn(obj) {
-    $.each(obj, function (i, d) {
-        if (typeof (d.replace) !== "undefined") { $(d.replace).html(d.data);}
-        else if (typeof (d.append) !== "undefined") { $(d.append).append(d.data);} 
-        else if (typeof (d.prepend) !== "undefined") { $(d.prepend).prepend(d.data);} 
-        else if (typeof (d.execute) !== "undefined") { eval(d.execute);}
-		else if (typeof (d.assign) !== "undefined") {eval (d.assign + "= d.data");}
-        else {alert(JSON.stringify(d));}
-    });
+  $.each(obj, function (i, d) {
+    if (typeof (d.replace) !== "undefined") { $(d.replace).html(d.data); } 
+    else if (typeof (d.append) !== "undefined") { $(d.append).append(d.data); } 
+    else if (typeof (d.prepend) !== "undefined") { $(d.prepend).prepend(d.data); } 
+    else if (typeof (d.execute) !== "undefined") { eval(d.execute); } 
+    else if (typeof (d.assign) !== "undefined") { eval (d.assign + "= d.data"); } 
+    else { alert(JSON.stringify(d)); }
+  });
 }
 
-function APLstringify(obj){
-	if (obj instanceof Date) {return obj.toString()}
-	else if ("object" === typeof obj) {return JSON.stringify(obj)}
-	else {return obj}
-	}
+function APLstringify(obj) {
+  if (obj instanceof Date) { return obj.toString(); } 
+  else if ("object" === typeof obj) {
+  	seen = [];
+    var replacer = function(key, value) {
+      if (value != null && typeof value == "object") {
+        if (seen.indexOf(value) >= 0) {
+          return;
+        }
+        seen.push(value);
+      }
+      return value;
+    };
+    return JSON.stringify(obj,replacer);
+  } else {
+    return obj;
+  }
+}
 
 function JSONSubset(model, selectors) {
-    if (selectors == "*") {
-        return JSON.stringify(model);
-    } else {
-        var arr = {};
-        var sel = selectors.split(" ");
-        for (var s in sel) {
-            if (model[sel[s]] === undefined) arr[sel[s]] = null;
-            else arr[sel[s]] = model[sel[s]];
-        }
-        return JSON.stringify(arr);
+  if (selectors == "*") { 
+  	return JSON.stringify(model); 
+  } 
+  else {
+    var arr = {};
+    var sel = selectors.split(" ");
+    for (var s in sel) {
+      if (model[sel[s]] === undefined) arr[sel[s]] = null;
+      else arr[sel[s]] = model[sel[s]];
     }
+    return JSON.stringify(arr);
+  }
 }
