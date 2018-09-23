470ea401e7bf8ff2a134d80e1810c78aa85ea992
Brian P Becker <brian@bluedolphinsolutions.com>
2018-09-23 15:20:38 -0400

    Remove Syncfusion v14

--- PlugIns/Syncfusion-14.3.0.49/assets/scripts/aurelia/es6/rangenavigator/rangeseries.js
+++
@@ -1,9 +0,0 @@
-import {inlineView, customElement, constants, generateBindables} from '../common/common';
-
-@inlineView(`${constants.aureliaTemplateString}`)
-@customElement(`${constants.elementPrefix}range-series`)
-@generateBindables('series', ['xName', 'yName', 'dataSource', 'type', 'enableAnimation', 'fill'])
-
-export class RangeSeries {
-}
-
